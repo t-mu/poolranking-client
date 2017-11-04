e9da255bbb212b57ecbf5ab4b9bacaad30594027
Johannes Valtonen <Johannes.Valtonen@Affecto.com>
2017-11-04 11:50:58 +0200

    Added endpoint for getting api urls.

--- server/app.js
+++ server/app.js
@@ -5,6 +5,14 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, '../', 'build')));
 
 const indexFilePath = path.resolve(__dirname, '../', 'build', 'index.html');
+
+app.get('/endpoints', (req, res) => {
+    res.json({
+        apiGatewayHost: process.env.POOLRANKING_API_GATEWAY_SERVICE_HOST,
+        apiGatewayPort: process.env.POOLRANKING_API_GATEWAY_SERVICE_PORT
+    });
+});
+
 app.get('/*', (req, res) => {
     res.sendFile(indexFilePath);
 });
