ceaab77ec9cd9a43db4de1fcef386d2a8cf2766c
kollimies <teemu.mantyharju@gmail.com>
2017-11-21 23:12:11 +0200

    add player view styles tuned + added variables etc. other small style fixing

--- src/components/AddPlayer/AddPlayer.tsx
+++ src/components/AddPlayer/AddPlayer.tsx
@@ -32,12 +32,40 @@ class AddPlayer extends React.Component<StateProps & DispatchProps> {
         this.userName = event.target.value;
     }
 
+    public submit(event: KeyboardEvent): void
+    {
+        if (event.keyCode === 13)
+        {
+            this.createPlayer();
+        }
+    }
+
     render() {
         return (
             <div className="AddPlayer">
-                <label htmlFor="username">Username:</label>
-                <input name="username" onChange={this.updateUsername.bind(this)} />
-                <button onClick={this.createPlayer.bind(this)}>Create player</button>
+                <div className="field">
+                    <label className="label">Username</label>
+                    <div className="control">
+                        <input
+                            className="input"
+                            type="text"
+                            placeholder="Insert username"
+                            name="username"
+                            autoFocus={true}
+                            onChange={this.updateUsername.bind(this)}
+                            onKeyDown={this.submit.bind(this)}
+                        />
+                    </div>
+                </div>
+                <div className="field">
+                    <div className="control">
+                        <button
+                            className="button is-primary"
+                            onClick={this.createPlayer.bind(this)}
+                        >Add new user!
+                        </button>
+                    </div>
+                </div>
             </div>
         );
     }
